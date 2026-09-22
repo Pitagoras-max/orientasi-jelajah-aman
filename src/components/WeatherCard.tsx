d69712bbb37d3deecbf6c0bd8cf42075679f53ca
M.Risqi Agung Pratama <risqiagung331@gmail.com>
2026-09-22 09:47:51 +0700

    feat: tambah type scale dan spacing system

--- src/components/WeatherCard.tsx
+++ src/components/WeatherCard.tsx
@@ -1,15 +1,19 @@
-// components/WeatherCard.tsx
 import { View, Text } from "react-native";
 import { WeatherCardProps } from "../../types/cuaca";
+
+// 1. Baris import untuk memanggil variabel dari styles.ts
+import { typeScale, spacing } from "../constants/styles";
 
 export default function WeatherCard({ kota, suhu, tingkatAQI }: WeatherCardProps) {
   const warnaAQI = tingkatAQI === "BAIK" ? "green" : "orange";
 
   return (
-    <View style={{ padding: 16, borderRadius: 8, backgroundColor: "#F4F7FA" }}>
-      <Text style={{ fontWeight: "bold", fontSize: 18 }}>{kota}</Text>
+    // 2. Menggunakan spacing.sedang
+    <View style={{ padding: spacing.sedang, borderRadius: 8, backgroundColor: "#F4F7FA" }}>
+      {/* 3. Menggunakan typeScale */}
+      <Text style={{ fontWeight: "bold", fontSize: typeScale.judul }}>{kota}</Text>
       <Text style={{ fontSize: 32 }}>{suhu}°C</Text>
-      <Text style={{ color: warnaAQI }}>AQI: {tingkatAQI}</Text>
+      <Text style={{ color: warnaAQI, fontSize: typeScale.isi }}>AQI: {tingkatAQI}</Text>
     </View>
   );
 }
